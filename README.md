45752c21307e8d3606a9c64fbe516cfcb0c844fe
marc.hoareau <marc.hoareau@esiroi.re>
2026-04-10 11:08:47 +0400

    fin de l'exercice 13

--- README.md
+++ README.md
@@ -5,3 +5,14 @@ Une implémentation des listes chainées en C.
 ## fork avril 2026
 
 Voici le lien du [git originel](https://github.com/payetdenis/liste-c "liste-c Payet Denis") que je vais reimplementer. (plus de details [içi](info_dev.md "info dev"))
+
+## API de la librairie liste-c
+
+createLC: Sert a creer une liste de type "ListeC"  
+sizeLC: Renvoie la taille de la liste  
+addLC: Ajoute une valeur a la fin de la liste  
+getLC: Donne la valeur attribué a une position dans la liste  
+replaceLC: Remplace la valeur a une position donné  
+insertLC: Insert une valeur a une position donné  
+removeLC: Suprime la position donné  
+deleteLC: Suprime la liste pour liberer la memoire  
--- src/liste-c.h
+++ src/liste-c.h
@@ -6,11 +6,13 @@ char* versionLC();
 struct st_listeC;
 typedef struct st_listeC * ListeC;
 
-ListeC createLC();					
-int sizeLC(ListeC liste);
-void addLC(ListeC liste, int value);	
-int getLC(ListeC liste, int pos);	
+ListeC createLC(); // Sert a creer une liste de type "ListeC"
+int sizeLC(ListeC liste); // Renvoie la taille de "liste"
+void addLC(ListeC liste, int value); // Ajoute une valeur "value" a la fin de "liste"
+int getLC(ListeC liste, int pos); // Donne la valeur attribue a "pos" dans "liste"
 
-void replaceLC(ListeC liste, int value, int pos);	
-void insertLC(ListeC liste, int value, int pos);	
-void removeLC(ListeC liste, int pos);
+void replaceLC(ListeC liste, int value, int pos); // Remplace la valeur a "pos" par "value"
+void insertLC(ListeC liste, int value, int pos); // Insert une valeur "value" a "pos"
+void removeLC(ListeC liste, int pos); // Suprime la position "pos"
+
+void deleteLC(ListeC liste); // Suprime "liste" pour liberer la memoire
