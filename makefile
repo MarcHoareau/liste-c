34fce28370f1a6c3b919e50bf1a2380a9e1c481f
marc.hoareau <marc.hoareau@esiroi.re>
2026-04-10 11:39:36 +0400

    fin de l'exercice 16

--- makefile
+++ makefile
@@ -14,11 +14,11 @@ lib : $(SRC)/liste-c.c
 test01 : $(TEST)/test01.c
 	gcc -Wall $(TEST)/test01.c $(LIB)/liste-c.o -o $(TEST)/test01 && $(TEST)/test01
 
-test02 : $(TEST)/test02.c
+test02 : $(TEST)/test02.c lib
 	gcc -Wall $(TEST)/test02.c $(LIB)/liste-c.o -o $(TEST)/test02 && $(TEST)/test02
 
 test03 : $(TEST)/test03.c
 	gcc -Wall $(TEST)/test03.c $(LIB)/liste-c.o -o $(TEST)/test03 && $(TEST)/test03
 
-bench_test02 : $(TEST)/test02.c
+bench_test02 : $(TEST)/test02.c lib
 	gcc -g -fsanitize=address -Wall $(TEST)/test02.c $(LIB)/liste-c.o -o $(TEST)/test02 && $(TEST)/test02
--- src/liste-c.c
+++ src/liste-c.c
@@ -9,3 +9,50 @@ struct st_listeC {
     NodeLC head;
     int size;
 };
+
+ListeC createLC(){
+    ListeC liste = malloc(sizeof(struct st_listeC));
+    if(liste == NULL) return NULL;
+
+    liste->head = NULL;
+    liste->size = 0;
+
+    return liste;
+}
+
+int sizeLC(ListeC liste){
+    return liste->size;
+}
+
+void addLC(ListeC liste, int value){
+    NodeLC nextNode = malloc(sizeof(struct st_nodeLC));
+    if(nextNode == NULL) return;
+
+    nextNode->value = value;
+    nextNode->next = NULL;
+
+    if(liste->head == NULL){
+        liste->head = nextNode;
+    } else {
+        NodeLC currentNode = liste->head;
+        while(currentNode->next != NULL){
+            currentNode = currentNode->next;
+        }
+        currentNode->next = nextNode;
+    }
+
+    liste->size++;
+}
+
+int getLC(ListeC liste, int pos){
+    if(pos < 0 || pos >= liste->size){
+        return -1;
+    }
+    NodeLC currentNode = liste->head;
+
+    for(int i = 0; i < pos; i++){
+        currentNode = currentNode->next;
+    }
+
+    return currentNode->value;
+}
